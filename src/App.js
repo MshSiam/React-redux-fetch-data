c5a8dde10c6e2ee34d2bd6becf77af511625880f
MshSiam <siyamkenway@gmail.com>
2022-07-25 10:29:53 +0600

    file installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch Data using React redux</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// install all package
+//  declare const in a separate file
+// aync action create
+// Reducer create
+// sotre create
+// provide store
+// use store
+// add some css
